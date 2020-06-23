a7e2519aa13f4133b5df6d621d13a5f5ec1a4cad
Fer <fernandoza@gmail.com>
2020-06-23 15:56:02 -0500

    feat: create examples for edit, show all and delete.
    Also apply dependency injection to use the adsDao

--- src/main/java/com/codeup/blog/controllers/AdsController.java
+++ src/main/java/com/codeup/blog/controllers/AdsController.java
@@ -1,5 +1,6 @@
 package com.codeup.blog.controllers;
 
+import com.codeup.blog.daos.AdsRepository;
 import com.codeup.blog.models.Ad;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -7,18 +8,21 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class AdsController {
 
+    // dependency injection
+    private AdsRepository adsDao;
+    public AdsController(AdsRepository adsRepository){
+        adsDao = adsRepository;
+    }
+
     @GetMapping("/ads")
 //    @RequestMapping(value = "/ads", method = RequestMethod.GET)
     public String index(Model model){
-        ArrayList<Ad> adsList = new ArrayList<>();
-        adsList.add(new Ad("PS1", "Used"));
-        adsList.add(new Ad("PS2", "Used"));
-        adsList.add(new Ad("PS4", "Used"));
-        adsList.add(new Ad("SNES", "Used"));
+        List<Ad> adsList = adsDao.findAll();
         model.addAttribute("noAdsFound", adsList.size() == 0);
         model.addAttribute("ads", adsList);
         return "ads/index";
@@ -39,8 +43,31 @@ public class AdsController {
 
     @PostMapping("/ads/create")
     @ResponseBody
-    public String save(){
+    public String save()
+    {
+        Ad newAd = new Ad("XBOX X","brand new");
+        adsDao.save(newAd);
         return "create a new ad";
+    }
+
+    @PutMapping("/ads/{id}/edit")
+    @ResponseBody
+    public String update(@PathVariable long id){
+        // find an ad
+        Ad foundAd = adsDao.getOne(id); // select * from ads where id = ?
+        // edit the ad
+        foundAd.setTitle("XBOX Series X");
+        foundAd.setDescription("holiday");
+        // save the changes
+        adsDao.save(foundAd); // update ads set title = ? where id = ?
+        return "ad updated";
+    }
+
+    @DeleteMapping("/ads/{id}/delete")
+    @ResponseBody
+    public String destroy(@PathVariable long id){
+        adsDao.deleteById(id);
+        return "ad deleted";
     }
 
 
