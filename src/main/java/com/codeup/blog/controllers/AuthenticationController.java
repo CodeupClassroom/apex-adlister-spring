42d86b5d17ebd34bf326cba9a5414bbf563f8700
Douglas Hirsh <douglas@codeup.com>
2020-07-01 11:07:25 -0500

    feat: Implement login and logout.

---
+++ src/main/java/com/codeup/blog/controllers/AuthenticationController.java
@@ -0,0 +1,12 @@
+package com.codeup.blog.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AuthenticationController {
+    @GetMapping("/login")
+    public String showLoginForm() {
+        return "users/login";
+    }
+}
