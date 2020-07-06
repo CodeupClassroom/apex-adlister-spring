3569649dc6bc40380a39748cb841ac004e17aa71
Fer <fernandoza@gmail.com>
2020-07-06 10:03:14 -0500

    feat: adds junit4 and the first sanity tests

--- src/test/java/com/codeup/blog/AdsIntegrationTests.java
+++ src/test/java/com/codeup/blog/AdsIntegrationTests.java
@@ -3,17 +3,21 @@ package com.codeup.blog;
 import com.codeup.blog.daos.AdsRepository;
 import com.codeup.blog.daos.UsersRepository;
 import com.codeup.blog.models.User;
-import org.aspectj.lang.annotation.Before;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
+import org.springframework.mock.web.MockHttpSession;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import javax.servlet.http.HttpSession;
 
+import static org.junit.Assert.assertNotNull;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
@@ -63,4 +67,29 @@ public class AdsIntegrationTests {
                 .getRequest()
                 .getSession();
     }
+
+    @Test
+    public void contextLoads() {
+        // Sanity Test, just to make sure the MVC bean is working
+        assertNotNull(mvc);
+    }
+
+    @Test
+    public void testIfUserSessionIsActive() throws Exception {
+        // It makes sure the returned session is not null
+        assertNotNull(httpSession);
+    }
+
+    @Test
+    public void testCreateAd() throws Exception {
+        // Makes a Post request to /ads/create and expect a redirection to the Ad
+        this.mvc.perform(
+                post("/ads/create").with(csrf())
+                        .session((MockHttpSession) httpSession)
+                        // Add all the required parameters to your request like this
+                        .param("title", "brand new mac")
+                        .param("description", "best buy sale"))
+                .andExpect(status().is3xxRedirection());
+    }
+
 }
