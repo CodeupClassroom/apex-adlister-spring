494d8159912b12e98e08ddadd0260203bacca273
Fer <fernandoza@gmail.com>
2020-08-21 16:21:09 -0500

    feat: create a Suite class to run all tests at once

---
+++ src/test/java/com/codeup/blog/AdlisterTest.java
@@ -0,0 +1,13 @@
+package com.codeup.blog;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+        AdsIntegrationTests.class,
+        UsersIntegrationTests.class
+})
+public class AdlisterTest {
+
+}
